cac2f65e016434db139306956d4e8116b2acb16a
Denis Zhitnyakov <zhitnyakovda@gmail.com>
2025-10-06 12:28:10 +0300

    test

--- src/logger.mjs
+++ src/logger.mjs
@@ -3,7 +3,7 @@
  * Выводит события в консоль браузера в формате JSON
  */
 
-const EVENT_KEY = 'videojs-vast-plugin-event-777';
+const EVENT_KEY = 'videojs-vast-plugin-event';
 
 export class Logger {
   /**
